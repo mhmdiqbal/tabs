e5b2ae6127ce6416c13dd4b8aae7121f40011a5f
Muhamad Iqbal <48789019+mhmdiqbal@users.noreply.github.com>
2021-09-04 09:24:28 +0700

    fix crash removelistener

--- src/views/BottomTabBar.tsx
+++ src/views/BottomTabBar.tsx
@@ -126,11 +126,11 @@ class TabBarBottom extends React.Component<BottomTabBarProps, State> {
 
   componentWillUnmount() {
     if (Platform.OS === 'ios') {
-      Keyboard.removeListener('keyboardWillShow', this._handleKeyboardShow);
-      Keyboard.removeListener('keyboardWillHide', this._handleKeyboardHide);
+      Keyboard.addListener('keyboardWillShow', this._handleKeyboardShow).remove();
+      Keyboard.addListener('keyboardWillHide', this._handleKeyboardHide).revove();
     } else {
-      Keyboard.removeListener('keyboardDidShow', this._handleKeyboardShow);
-      Keyboard.removeListener('keyboardDidHide', this._handleKeyboardHide);
+      Keyboard.addListener('keyboardDidShow', this._handleKeyboardShow).remove();
+      Keyboard.addListener('keyboardDidHide', this._handleKeyboardHide).remove();
     }
   }
 
